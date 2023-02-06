a1314101054c7884a70620d4ff40128a033297e2
Terran Kartik Tellus <91951646+TerranKartikTellus@users.noreply.github.com>
2023-02-06 13:38:43 +0530

    save

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }) {
   }, [authUser, loading])
   console.log(authUser);
 
-  return <div className={router.asPath.includes('tool') ? 'bg-gray-900 text-gray-100 w-full h-full' : 'w-full h-full'}>
+  return <div className={router.asPath.includes('tool') ? 'bg-gray-900 text-gray-100 w-full h-full overflow-y-hidden' : 'w-full h-full overflow-y-hidden '}>
     <AuthUserProvider className="">
     <Nav></Nav>
     <div className='flex flex-row h-full items-start justify-between font-sans font-medium'> 
--- src/pages/tool/chat/index.js
+++ src/pages/tool/chat/index.js
@@ -6,12 +6,15 @@ import toast, { Toaster } from 'react-hot-toast';
 import firebase from 'firebase';
 import axios from 'axios';
 import { Configuration, OpenAIApi } from 'openai'
+// import openai
 const api =process.env.NEXT_PUBLIC_OPENAI_API
-
+const org =process.env.NEXT_PUBLIC_OPENAI_ORG
+// const openai = require("openai");
 export default function Edit({student,id}){
   const configuration = new Configuration({
     apiKey: api,
   });
+  console.log(api)
   const openai = new OpenAIApi(configuration);
 
     const { authUser, loading, signOut } = useAuth();
@@ -28,67 +31,30 @@ export default function Edit({student,id}){
 
 async function onSubmit(){
   setload(<Load></Load>)
-  // console.log(process.env.NEXT_PUBLIC_OPENAI_API);
-  // console.log(input);
-const DEFAULT_PARAMS = {
-  "model": "text-davinci-002",
-  "temperature": 0.7,
-  "max_tokens": 256,
-  "top_p": 1,
-  "frequency_penalty": 0,
-  "presence_penalty": 0
-}
-const requestOptions = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + String(api)
-    },
-    body: JSON.stringify({
-      'model': 'text-davinci-002',    
-      'prompt': input,
-          'temperature': 0.1,
-          'max_tokens': 50,
-          'top_p': 1,
-          'frequency_penalty': 0,
-          'presence_penalty': 0.5,
-          'stop': ["\"\"\""],
-        })
-      
-  };
-  await fetch('https://api.openai.com/v1/completions', requestOptions)
-  
-  // await openai.createCompletion({
-  //     model: "text-davinci-003",
-  //     prompt: `${input}`,
-  //     temperature: 0, // Higher values means the model will take more risks.
-  //     max_tokens: 50, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-  //     top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-  //     frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-  //     presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
-  //   })
 
-  .then(function (response) {
-    console.log("Summary: " + response.data.choices[0].text);
-    setoutput(response.data.choices[0].text)
-    setoutput('got')
-  
-  })
-  .catch(function (error) {
-    console.log('Server error'+error);
-    setoutput('Error Occured')
-  
-  });
+ const configuration = new Configuration({
+    organization: org,
+    apiKey: api,
+});
+const openai = new OpenAIApi(configuration);
+
+const completion = await openai.createCompletion({
+  model: "text-davinci-002",
+  prompt: input,
+});
+console.log(completion.data.choices[0].text);
+completion.data && setoutput(completion.data.choices[0].text && completion.data.choices[0].text);
 
   setload("Get")
 
 }
 
 return(
-    <div className='w-full h-full flex flex-col items-center justify-center space-y-4'>
+    <div className='w-full  flex flex-col items-center justify-center space-y-4 h-screen'>
 
     <div className='flex flex-row items-center justify-center space-x-3 w-full '>
-      <input placeholder='Enter Short Text for Summarization' className='placeholder-gray-900 focus:border-[2px] focus:border-green-500 outline-0 p-2 w-5/12        text-gray-900 rounded-md h-[250px]'        onChange={(e)=>{setinput(e.target.value)}} ></input>
+      <textarea placeholder='Enter Short Text for Summarization' className='overflow-clip  text-ellipsis placeholder-gray-900 focus:border-[2px] focus:border-green-500 outline-0 p-2 w-5/12        text-gray-900 rounded-md h-[250px]'        onChange={(e)=>{setinput(e.target.value)}} ></textarea>
+      
       <div  placeholder='Summarized text will appear here  ' className={`${load=='Get' && "border-[1px] border-green-600 text-gray-100"} placeholder-gray-100 outline-0 p-2 w-5/12  rounded-md h-[250px]`} >{output}</div>
     </div>
     <button onClick={onSubmit} className={` bg-green-600 rounded-sm text-gray-200 py-1 px-20 `}>{load}</button>
